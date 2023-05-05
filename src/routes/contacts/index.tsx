a0c09ff12ec869653726fbde1952f38b801e1735
lkb <tmwlak@gmail.com>
2023-05-05 23:32:03 +0900

    -

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -1,12 +1,35 @@
 import { component$ } from "@builder.io/qwik";
+import {CONTACTS, } from './fake-api'
+import { routeAction$, routeLoader$ } from "@builder.io/qwik-city";
 
-const data = [1,2,3,4]
+export const useRouteLoader = routeLoader$(() => {
+    return CONTACTS
+})
+
+export const useRouteActions = routeAction$(() => {
+
+})
 
 export default component$(() => {
+
+    const loaded = useRouteLoader()
+    // const routeactions = useRouteActions()
     return (
         <div class="asdf">
-            hi
-            {data.map(v => <><div class="">{v}</div></>)}
+            {loaded.value.map((v) => 
+                <>
+                    <div class="1" key={v.id}>
+                    
+                        <div class="">{v.name}</div>
+                        <img src={v.avatar} />
+                        <a href={v.github} >github</a>
+                        <a href={v.twitter} >twitter</a>
+                        <a href={v.linkedin} >linkedin</a>
+                        {/* <button onClick$={routeactions}>route actions</button> */}                        
+                    </div> 
+                </>
+            )}
+            {/* {data.map(v => <><div class="">{v}</div></>)} */}
         </div>
     )
 }) 
